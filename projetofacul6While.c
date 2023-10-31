767c963fd43bebc97ec6f02becbb976d81c2eba5
Paulorabeloo <pauloeduardorabelomiranda@gmail.com>
2023-10-31 17:03:04 -0300

    projetofacul6While

---
+++ projetofacul6While.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+int numero;
+numero = 1;
+
+while(numero <=5){                              // ENQUANTO (LAÇO DE REPETIÇÃO)
+    printf("%i \n \n", numero);
+    numero++;                                  // numero = numero + 1;
+}
+return 0;
+}
